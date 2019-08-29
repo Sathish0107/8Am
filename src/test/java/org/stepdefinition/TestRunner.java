4ae06428e046f9e2ace496b32bd963f252100c26
Sathish0107 <sathishbaskaranr@gmail.com>
2019-08-29 08:57:48 +0530

    29/8/19

--- src/test/java/org/stepdefinition/TestRunner.java
+++ src/test/java/org/stepdefinition/TestRunner.java
@@ -8,11 +8,13 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",
                   glue="org.stepdefinition",
-                  plugin= {"html:target","rerun:src/test/resources/failed.txt"},
+                  plugin= {"html:target","rerun:src/test/resources/failed.txt","json:target/report.json"},
                   monochrome=true,
                   dryRun=false,
-                  tags= {"~@karthi"})
+                  tags= {"~@karthi"},
+                  strict=false)
 
 public class TestRunner {
 
+	
 }
